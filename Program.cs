067fc519d92f8bedbd2a8f8cec291ed76c6e8a2e
bbayrakli <bayrakliburak@outlook.com>
2019-06-10 13:34:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double x;
+            Console.WriteLine("bir sayı giriniz:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
